baef75dc7a48cbb4817c960a2a06173830a9ddb1
unknown <octamarina@gmail.com>
2021-11-20 14:43:34 +0200

    added FriendshipRequestValidator

--- src/validators/FriendshipRequestValidator.java
+++ src/validators/FriendshipRequestValidator.java
@@ -2,6 +2,8 @@ package validators;
 
 
 import domain.FriendshipRequest;
+import exceptions.FriendshipRequestException;
+import exceptions.UserException;
 
 public class FriendshipRequestValidator implements StrategyValidator<FriendshipRequest>{
     private final static FriendshipRequestValidator friendshipRequestValidator=new FriendshipRequestValidator();
@@ -10,7 +12,27 @@ public class FriendshipRequestValidator implements StrategyValidator<FriendshipR
     public static FriendshipRequestValidator getInstance(){return friendshipRequestValidator;}
 
     @Override
-    public void validate(FriendshipRequest friendshipRequest){
+    public void validate(FriendshipRequest friendshipRequest)throws FriendshipRequestException {
+        String err = "";
+        if(friendshipRequest.getId()<0)
+            err += "Invalid ID!\n";
 
+        try{
+            UserValidator.getInstance().validate(friendshipRequest.getSender());
+        } catch(UserException error){
+            err += "Sender: \n";
+            err += error.getMessage();
+        }
+
+        try{
+            UserValidator.getInstance().validate(friendshipRequest.getReceiver());
+        } catch(UserException error){
+            err += "Receiver: \n";
+            err += error.getMessage();
+        }
+
+        if(!err.equals(""))
+            throw new FriendshipRequestException(err);
     }
 }
+
