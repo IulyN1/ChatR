f74ee4bf8f163a1c2e1c70bf521b9194f9f18a32
Octavian Marina <octamarina@gmail.com>
2021-12-21 18:48:39 +0200

    added event validator

---
+++ src/main/java/com/example/chatr/validators/EventValidator.java
@@ -0,0 +1,33 @@
+package com.example.chatr.validators;
+
+import com.example.chatr.domain.Account;
+import com.example.chatr.domain.Event;
+import com.example.chatr.domain.FriendshipRequest;
+import com.example.chatr.exceptions.AccountException;
+
+public class EventValidator  implements StrategyValidator<Event> {
+    private final static EventValidator eventValidator= new EventValidator();
+
+    private EventValidator() {
+    }
+    public static EventValidator getInstance() {
+        return eventValidator;
+    }
+
+
+    @Override
+    public void validate(Event event) throws Exception {
+        String err = "";
+        if (event.getName() == null)
+            err = err + "Empty event!\n";
+        if (event.getDate() == null)
+            err = err + "Empty  date!\n";
+        if (event.getId() < 0)
+            err = err + "Invalid id!\n";
+        if (!err.equals(""))
+            throw new AccountException(err);
+    }
+}
+
+
+}
