98ba7f7bf409e777f74770dedfeb1fe22569f885
Octavian Marina <octamarina@gmail.com>
2021-12-21 18:44:00 +0200

    added event entity

---
+++ src/main/java/com/example/chatr/domain/Event.java
@@ -0,0 +1,77 @@
+package com.example.chatr.domain;
+
+import java.util.Objects;
+
+public class Event extends Entity<Integer>  {
+    String name;
+    String date;
+    String subscribers;
+
+    public Event(Integer integer, String name, String date, String subscribers) {
+        super(integer);
+        this.name = name;
+        this.date = date;
+        this.subscribers = subscribers;
+    }
+    public Event(String name, String date, String subscribers) {
+        super(0);
+        this.name = name;
+        this.date = date;
+        this.subscribers = subscribers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(String subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return name.equals(event.name) && date.equals(event.date) && subscribers.equals(event.subscribers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, date, subscribers);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", subscribers='" + subscribers + '\'' +
+                '}';
+    }
+
+    @Override
+    public void modify(Entity<Integer> entity) {
+        Event event = (Event) entity;
+        setName(event.getName());
+        setSubscribers(event.getSubscribers());
+        setDate(event.getDate());
+    }
+}
