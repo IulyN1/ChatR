18d36f37e4cb316d17f25bbfd3e100b3f96e5b53
Iulian Popa <iulianp14@yahoo.com>
2022-01-05 11:35:28 +0200

    Added password hashing and storing the hashed password in the database

--- src/main/java/com/example/chatr/service/ServiceAccount.java
+++ src/main/java/com/example/chatr/service/ServiceAccount.java
@@ -1,5 +1,6 @@
 package com.example.chatr.service;
 
+import com.example.chatr.controllers.CreateAccountController;
 import com.example.chatr.domain.Account;
 import com.example.chatr.domain.User;
 import com.example.chatr.exceptions.AccountException;
@@ -7,6 +8,12 @@ import com.example.chatr.exceptions.RepoException;
 import com.example.chatr.repository.Repo;
 import com.example.chatr.validators.StrategyValidator;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 import java.util.Collection;
 
 public class ServiceAccount {
@@ -44,8 +51,9 @@ public class ServiceAccount {
 
     public Account verifyAccount(String username, String password) throws Exception {
         Collection<Account> accounts = getAllAccounts();
+        String hashedPassword = hashPassword(username,password);
         for (Account ac : accounts)
-            if (ac.getUsername().equals(username) && ac.getPassword().equals(password)) {
+            if (ac.getUsername().equals(username) && ac.getPassword().equals(hashedPassword)) {
                 return ac;
             }
         throw new AccountException("Invalid username or password!");
@@ -61,5 +69,20 @@ public class ServiceAccount {
                 throw new AccountException("Username already in use!");
             }
         }
+    }
+
+    /**
+     * PBKDF2 hashing for storing the hashed password in database
+     * @param username String
+     * @param password String
+     * @return the hashed password
+     */
+    public String hashPassword(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(),username.getBytes(),65536,128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        Base64.Encoder enc = Base64.getEncoder();
+        System.out.println(enc.encodeToString(hash));
+        return enc.encodeToString(hash);
     }
 }
